104c892a544b3ad4e9ca7b2c8e07d4d0b636aef7
Imaran Rahman <imranrahman543@gmail.com>
2022-10-21 23:56:12 +0600

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,10 +1,11 @@
-#include "initializer.h"
-
+//#include "initializer.h"
+#include "logic.h"
 int width = 1080;
 int height = 720;
 
 int x_pos = SDL_WINDOWPOS_CENTERED;
 int y_pos = SDL_WINDOWPOS_CENTERED;
+
 
 int main ()
 
@@ -12,6 +13,12 @@ int main ()
 {
 
     bool x= game_intialization("WORDS",x_pos, y_pos, width, height);
+    logic();
+
+        SDL_Delay(15000);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
 
    
 }
