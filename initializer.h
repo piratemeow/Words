7ffca677a06706c818fb0d19b3c8e274e903f52e
Imaran Rahman <imranrahman543@gmail.com>
2022-11-03 03:38:13 +0600

    Update initializer.h

--- initializer.h
+++ initializer.h
@@ -2,11 +2,17 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 #include <stdio.h>
+#include <string>
+#include <iostream>
 #include "constants.h"
-
+using namespace std;
 bool game_intialization(const char *title, int x_pos, int y_pos,int width, int height)
 {
     // Initializing SDL
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)!=0){
+        printf("Error");
+    }
+
     if (SDL_Init(SDL_INIT_EVERYTHING)!=0)
     {
         printf("ERROR INITIALIZING SDL.\n");
@@ -47,10 +53,12 @@ bool game_intialization(const char *title, int x_pos, int y_pos,int width, int h
         IMG_Quit();
         return 0;
     }
-
     
-    surface = IMG_Load("6.png");
-
+   
+   
+    
+    surface = IMG_Load("background.png");
+   // surface = IMG_Load("0.png");
     if (!surface)
     {
         printf ("ERROR SURFACING IMAGE\n");
@@ -60,30 +68,79 @@ bool game_intialization(const char *title, int x_pos, int y_pos,int width, int h
         return 0;
     }
     
-    SDL_Texture * tex= SDL_CreateTextureFromSurface(renderer,surface);
+    
+
+    // if (!tex)
+    // {
+    //     printf("ERROR CREATING TEXTURE\n");
+
+    //     SDL_DestroyRenderer(renderer);
+    //     SDL_DestroyWindow(window);
+    //     SDL_Quit();
+    //     return 0;
+
+    // }
+
+    // SDL_Texture *back ;
+   
+    back = SDL_CreateTextureFromSurface (renderer,surface);
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer,back,NULL,NULL);
+    SDL_RenderPresent (renderer);
+
+    // SDL_Texture * tex[7];
+    surface = IMG_Load("0.png");
+    tex[0] = SDL_CreateTextureFromSurface(renderer,surface);
     SDL_FreeSurface(surface);
 
-    if (!tex)
-    {
-        printf("ERROR CREATING TEXTURE\n");
+    surface = IMG_Load("1.png");
+    tex[1] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
 
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 0;
+    surface = IMG_Load("2.png");
+    tex[2] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
+
+    surface = IMG_Load("3.png");
+    tex[3] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
+
+    surface = IMG_Load("4.png");
+    tex[4] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
+    
+    surface = IMG_Load("5.png");
+    tex[5] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
+
+    surface = IMG_Load("6.png");
+    tex[6] = SDL_CreateTextureFromSurface(renderer,surface);
+    SDL_FreeSurface(surface);
+
+
+    // surface = TTF_RenderText_Solid(font,"_",{0,0,0});
+    // SDL_Texture *under_score = SDL_CreateTextureFromSurface(renderer,surface);
+    // SDL_FreeSurface(surface);
+
+    //SDL_Rect dest[7];
+    SDL_QueryTexture(tex[0],NULL,NULL,&dest[0].w,&dest[0].h);
+
+    for (int i=0; i<7 ;i++)
+    {
+    dest[i].w=(int) dest[i].w *1;
+    dest[i].h = (int) dest[i].h *1;
+    dest[i].x= (int) 750;
+    dest[i].y = 0 ;
+    
     }
 
-    SDL_Rect dest[2];
-    SDL_QueryTexture(tex,NULL,NULL,&dest[0].w,&dest[0].h);
+   // SDL_RenderClear(renderer); this was changed
 
-    dest[0].w=(int) dest[0].w *1;
-    dest[0].h = (int) dest[0].h *1;
-    dest[0].x= (int) 900;
-    dest[0].y = 0 ;
+   SDL_RenderCopy(renderer,tex[0],NULL,&dest[0]);
 
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer,tex,NULL,&dest[0]);
-    SDL_RenderPresent(renderer);
+
+    
+    //SDL_RenderPresent(renderer);
 
 
     // SDL_Delay(15000);
