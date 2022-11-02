8e78e0e754add3dca37419377dd4215e2982a5e3
Imaran Rahman <imranrahman543@gmail.com>
2022-11-03 03:37:11 +0600

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,3 +1,9 @@
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Surface *surface = NULL;
+SDL_Event c;
+SDL_Texture *back ;
+SDL_Rect dest[7];
+SDL_Texture * tex[7];
+bool quit = 1;
+// TTF_Font *font = TTF_OpenFont("montserrat/Montserrat-Black.otf",16);
