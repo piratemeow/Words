39a6e6cdda145e88d7e6e486edcf222b7deb7d36
Imaran Rahman <imranrahman543@gmail.com>
2022-12-29 18:30:56 +0600

    Update constants.h

--- constants.h
+++ constants.h
@@ -7,9 +7,14 @@ SDL_Rect life[5];
 SDL_Texture * heart;
 SDL_Texture *Next_round;
 SDL_Rect next_round;
- SDL_Event e;
+SDL_Event e;
+SDL_Rect menu;
+SDL_Texture *menuee;
+SDL_Rect ans;
+SDL_Texture *correct = NULL;
+SDL_Texture *incorrect = NULL;
+
+SDL_Rect hint;
+SDL_Texture *hinti = NULL;
 bool quit = 1;
 bool game = 1;
-//SDL_Event mouse;
-// TTF_Font *font = TTF_OpenFont("montserrat/Montserrat-Medium.otf",20);
-// TTF_Font *font = TTF_OpenFont("montserrat/Montserrat-Black.otf",16);
