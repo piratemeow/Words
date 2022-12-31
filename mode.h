0f9cb0a0fab496c1727efb28d1cb3123f013144f
Imaran Rahman <imranrahman543@gmail.com>
2022-12-31 20:02:59 +0600

    Update mode.h

--- mode.h
+++ mode.h
@@ -1,94 +1,159 @@
 
-void mode_screen()
+void buttonShow(SDL_Renderer *&rend, SDL_Texture *text[], SDL_Rect dest[], SDL_Texture *&bg, bool flag, int idx)
 {
-	TTF_Init();
-	TTF_Font *font = TTF_OpenFont("montserrat/Montserrat-Medium.otf",20);
-	surface = TTF_RenderText_Solid(font,"KIDS MODE",{0,0,0});
-    SDL_Texture *kids[2];
-    kids[0] = SDL_CreateTextureFromSurface(renderer,surface);
-    kids[1] = SDL_CreateTextureFromSurface(renderer,surface);
-    SDL_FreeSurface(surface);
-	surface = TTF_RenderText_Solid(font,"QUIT",{0,0,0});
-	SDL_Texture *quit = SDL_CreateTextureFromSurface(renderer,surface);
-	SDL_FreeSurface(surface);
-    SDL_Rect kid[2];
-    kid[0].w = 200;
-    kid[0].h = 150;
-    kid[0].x = 500;
-    kid[0].y = 100;
-	
-	SDL_Rect quit_button;
-    quit_button.w = 150;
-    quit_button.h = 100;
-    quit_button.x = 530;
-    quit_button.y = 500;
-	//SDL_RenderCopy(renderer,quit,NULL,&quit_button);
-    SDL_RenderCopy(renderer,kids[0],NULL,&kid[0]);
+	// This function will display the buttons. In this case, if the cursor comes to an option, the colour of that option will be changed. If the cursor is moved away, then the colour is changed again and gets its previous look.
 
-	surface = TTF_RenderText_Solid(font,"NORMAL MODE",{0,0,0});
-    SDL_Texture *normal[2];
-    normal[0] = SDL_CreateTextureFromSurface(renderer,surface);
-    
+	int i;
+	SDL_RenderClear(rend);
 
-	SDL_Rect normal_button;
-	normal_button.w = 220;
-	normal_button.h = 150;
-	normal_button.x = 490;
-	normal_button.y = 300;
-	SDL_RenderCopy(renderer,normal[0],NULL,&normal_button);
-	
-	SDL_RenderPresent(renderer);
-	return ;
+	SDL_RenderCopy(rend, bg, NULL, NULL);
+
+	SDL_SetRenderDrawColor(rend, 60, 50, 180, 170);
+
+	for (i = 0; i < idx; i++)
+	{
+		SDL_RenderFillRect(rend, &dest[i]);
+	}
+	if (!flag)
+	{
+		SDL_SetRenderDrawColor(rend, 0, 255, 0, 255); // if flag is false then the colour will be blue
+		SDL_RenderFillRect(rend, &dest[idx]);		  // if flag is true then the colour will be green
+		SDL_SetRenderDrawColor(rend, 60, 50, 180, 170);
+	}
+	else
+	{
+		SDL_RenderFillRect(rend, &dest[idx]);
+	}
+	for (i = idx + 1; i < 3; i++)
+	{
+		SDL_RenderFillRect(rend, &dest[i]);
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		SDL_RenderCopy(rend, text[i], NULL, &dest[i]);
+	}
+	SDL_RenderPresent(rend);
 }
 
-
-
-int mode ()
+void mode_screen(SDL_Renderer *&renderer, TTF_Font *&font, SDL_Texture *text[], SDL_Rect des[], SDL_Texture *&bg)
 {
-    mode_screen();
-    
+	// This function will load all the mode ( “Kids mode” or “Normal mode” ) options on the window.
 
-    SDL_Event mode;
-    while (game)
-    {
-        int x,y;
-        SDL_PollEvent(&mode);
-		if(mode.type==SDL_QUIT)
+	surface = TTF_RenderText_Solid(font, "KIDS MODE", {255, 0, 255});
+	text[0] = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	surface = TTF_RenderText_Solid(font, "QUIT", {255, 0, 255});
+	text[2] = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	SDL_Rect full_screen;
+	full_screen.w = 1200;
+	full_screen.h = 800;
+	full_screen.x = 0;
+	full_screen.y = 0;
+
+	des[0].w = 200;
+	des[0].h = 90;
+	des[0].x = 500;
+	des[0].y = 100;
+
+	des[2].w = 150;
+	des[2].h = 70;
+	des[2].x = 530;
+	des[2].y = 500;
+
+	des[1].w = 220;
+	des[1].h = 90;
+	des[1].x = 490;
+	des[1].y = 300;
+
+	SDL_SetRenderDrawColor(renderer, 135, 206, 235, 170);
+	SDL_RenderFillRect(renderer, &full_screen);
+	surface = IMG_Load("Alphabet_image/background.png");
+	bg = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	SDL_RenderCopy(renderer, bg, NULL, NULL);
+	SDL_SetRenderDrawColor(renderer, 60, 50, 180, 170);
+	SDL_RenderFillRect(renderer, &des[0]);
+	SDL_RenderFillRect(renderer, &des[2]);
+	SDL_RenderFillRect(renderer, &des[1]);
+	SDL_RenderCopy(renderer, text[2], NULL, &des[2]);
+	SDL_RenderCopy(renderer, text[0], NULL, &des[0]);
+
+	surface = TTF_RenderText_Solid(font, "NORMAL MODE", {255, 0, 255});
+	text[1] = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_RenderCopy(renderer, text[1], NULL, &des[1]);
+
+	SDL_RenderPresent(renderer);
+	return;
+}
+
+int mode(SDL_Renderer *&renderer, TTF_Font *&font, bool &quit)
+{
+	// Here the player will choose whether he will play “Kids mode” or “Normal mode”.
+	// In “kids mode” some letters of the word will be given along with a clue statement. So, this is a mode of less difficulty.
+	// On the other hand, in “Normal mode” only the clue statement will be given but no letter would be visible. So this is a comparatively difficult level.
+
+	SDL_Texture *text[3];
+	SDL_Rect des[3];
+	SDL_Texture *bg;
+
+	mode_screen(renderer, font, text, des, bg);
+
+	surface = IMG_Load("Alphabet_image/background.png");
+	bg = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+
+	SDL_Event mode;
+	bool close = false;
+	bool flag[3] = {false, false, false};
+	int option = 10;
+	while (!close)
+	{
+		int x, y;
+		SDL_PollEvent(&mode);
+		SDL_GetMouseState(&x, &y);
+
+		if (mode.type == SDL_QUIT)
 		{
+			quit = true;
 			game = 0;
 			return 0;
 		}
-		if (mode.type==SDL_MOUSEMOTION)
+
+		for (int i = 0; i < 3; i++)
 		{
-			
-			SDL_GetMouseState(&x,&y);
-			
-		}
-			if (SDL_MOUSEBUTTONUP==mode.type && SDL_BUTTON_LEFT==mode.button.button)
+			if (x >= des[i].x && x <= (des[i].x + des[i].w) && y >= des[i].y && y <= (des[i].y + des[i].h))
 			{
-				
-				
-				//return 1;
-				if (x>=500 && x<=(500+200) && y<=(100+150) && y>=100)
+				if (!flag[i])
 				{
-					
-                    
-					return 0;
+					buttonShow(renderer, text, des, bg, flag[i], i); // display buttons
+					flag[i] = true;
 				}
-
-				else if (x>=420 && x<=420+200 && y<=100+500 && y>=500)
+				if (mode.type == SDL_MOUSEBUTTONUP)
 				{
-					return -1;
+					option = i;
+					if (option == 0)
+						return 0; // kids mode
+					if (option == 1)
+						return 1; // normal mode
+					if (option == 2)
+						return -1; // quit
+					close = true;
 				}
-
-				else if (x>=490 && x<=420+220 && y<=150+300 && y>=300)
-				{
-					return 1;
-				}
-				
 			}
-		
-    }
-
-    return 10;
+			else
+			{
+				if (flag[i])
+				{
+					buttonShow(renderer, text, des, bg, flag[i], i);
+					flag[i] = false;
+				}
+			}
+		}
+	}
+	return 10;
 }
